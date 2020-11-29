a4cb96d30b3fcff3afd5ba1bea36b317c50e45fb
Rui Leite <ruipimentelleite@gmail.com>
2020-11-29 09:36:30 -0300

    Demonstra o `switch() ... case ... default`.

--- index.js
+++ index.js
@@ -10,10 +10,34 @@ let mensagemBoasVindas = ", caro(a) usuário(a)!";
  */
 const agora = new Date();
 
-if (agora.getHours() < 12) {
-  mensagemBoasVindas = `Bom dia${mensagemBoasVindas}`;
-} else {
-  mensagemBoasVindas = `Boa tarde${mensagemBoasVindas}`;
+switch (agora.getHours()) {
+  case 0:
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    mensagemBoasVindas = `Boa madrugada${mensagemBoasVindas}`;
+    break;
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+  case 11:
+    mensagemBoasVindas = `Bom dia${mensagemBoasVindas}`;
+    break;
+  case 12:
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+    mensagemBoasVindas = `Boa tarde${mensagemBoasVindas}`;
+    break;
+  default:
+    mensagemBoasVindas = `Boa noite${mensagemBoasVindas}`;
+    break;
 }
 
 // Imprime no documento HTML de modo síncrono:
