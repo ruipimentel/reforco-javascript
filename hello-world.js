473f5677a12acdfeabffa956fdd2805330073a7a
Rui Leite <ruipimentelleite@gmail.com>
2020-11-29 11:04:25 -0300

    Cria o módulo `hello-world.js`.

---
+++ hello-world.js
@@ -0,0 +1,41 @@
+/**
+ * Gera o cumprimento adequado, dentre os possíves listados a seguir,
+ * de acordo com a hora atual.
+ * * `'Boa madrugada'`
+ * * `'Bom dia'`
+ * * `'Boa tarde'`
+ * * `'Boa noite'`
+ */
+export const geraCumprimento = function() {
+
+  /**
+   * Data e hora do momento em que a declaração for executada.
+   */
+  const agora = new Date();
+
+  switch (agora.getHours()) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      return `Boa madrugada`;
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+      return `Bom dia`;
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+      return `Boa tarde`;
+    default:
+      return `Boa noite`;
+  }
+};
--- index.js
+++ index.js
@@ -1,44 +1,4 @@
-/**
- * Gera o cumprimento adequado, dentre os possíves listados a seguir,
- * de acordo com a hora atual.
- * * `'Boa madrugada'`
- * * `'Bom dia'`
- * * `'Boa tarde'`
- * * `'Boa noite'`
- */
-const geraCumprimento = function() {
-
-  /**
-   * Data e hora do momento em que a declaração for executada.
-   */
-  const agora = new Date();
-
-  switch (agora.getHours()) {
-    case 0:
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-      return `Boa madrugada`;
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 10:
-    case 11:
-      return `Bom dia`;
-    case 12:
-    case 13:
-    case 14:
-    case 15:
-    case 16:
-    case 17:
-      return `Boa tarde`;
-    default:
-      return `Boa noite`;
-  }
-};
+import { geraCumprimento } from './hello-world.js';
 
 /**
  * Mensagem de boas vindas para o(a) usuário(a).
@@ -48,4 +8,4 @@ const geraCumprimento = function() {
 const mensagemBoasVindas = `${geraCumprimento()}, caro(a) usuário(a)!`;
 
 // Imprime no documento HTML de modo síncrono:
-document.write(`<b>${mensagemBoasVindas}</b>`);
+console.log(`<b>${mensagemBoasVindas}</b>`);
