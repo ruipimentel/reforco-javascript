656b221db347cbb5f4bd73b2d5efdcdecf7a127b
Rui Leite <ruipimentelleite@gmail.com>
2020-11-29 09:17:41 -0300

    Demonstra a importação de um script externo no HTML e também o comentário de documentação (JSDoc).

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Mensagem de boas vindas para o(a) usuário(a).
+ */
+const mensagemBoasVindas = "Hello world!";
+
+// Imprime no documento HTML de modo síncrono:
+document.write(`<b>${mensagemBoasVindas}</b>`);
