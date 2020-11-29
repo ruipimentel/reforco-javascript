cc6905682d25211e5d2fc2438ad4ef44a0a36e16
Rui Leite <ruipimentelleite@gmail.com>
2020-11-29 09:28:43 -0300

    Mostra uma declaração de variável com `let`.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 /**
  * Mensagem de boas vindas para o(a) usuário(a).
+ *
+ * Dependerá da hora de acesso do(a) usuário(a).
  */
-const mensagemBoasVindas = "Hello world!";
+let mensagemBoasVindas = ", caro(a) usuário(a)!";
+
+/**
+ * Data e hora do momento em que a declaração for executada.
+ */
+const agora = new Date();
+
+if (agora.getHours() < 12) {
+  mensagemBoasVindas = `Bom dia${mensagemBoasVindas}`;
+} else {
+  mensagemBoasVindas = `Boa tarde${mensagemBoasVindas}`;
+}
 
 // Imprime no documento HTML de modo síncrono:
 document.write(`<b>${mensagemBoasVindas}</b>`);
